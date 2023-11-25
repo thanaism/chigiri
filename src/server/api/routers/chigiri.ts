adea46e9ae127502269da7935169ab0c8b44b89a
thanai <62784012+thanaism@users.noreply.github.com62784012+thanaism@users.noreply.github.com>
2023-11-25 17:06:56 +0900

    add: chigiriモデルに関するAPIを追加

---
+++ src/server/api/routers/chigiri.ts
@@ -0,0 +1,38 @@
+import axios from 'axios';
+import { z } from 'zod';
+import { env } from '~/env';
+
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
+
+export const chigiriRouter = createTRPCRouter({
+    get: publicProcedure.input(z.object({ id: z.string() })).query(({ ctx, input }) => {
+        return ctx.db.chigiri.findUnique({
+            where: { id: input.id },
+        });
+    }),
+
+    create: publicProcedure.input(z.object({ idToken: z.string().min(1) })).mutation(async ({ ctx, input }) => {
+        const { idToken } = input;
+        const userId = await getUserId(idToken);
+
+        return ctx.db.chigiri.create({
+            data: {
+                sender: { connectOrCreate: { where: { id: userId }, create: { id: userId } } },
+            },
+        });
+    }),
+
+    getLatest: publicProcedure.query(async ({ ctx }) => {
+        return ctx.db.chigiri.findFirst({
+            orderBy: { createdAt: 'desc' },
+        });
+    }),
+});
+
+const getUserId = async (idToken: string) => {
+    const headers = { 'Content-Type': 'application/x-www-form-urlencoded' };
+    const payload = { id_token: idToken, client_id: env.NEXT_PUBLIC_LIFF_ID.split('-')[0] };
+    const url = 'https://api.line.me/oauth2/v2.1/verify';
+    const res = await axios.post<{ sub: string }>(url, payload, { headers });
+    return res.data.sub;
+};
