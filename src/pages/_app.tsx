aa2b8c7d334b76ac3ef73713aaa4864520000f1b
thanai <62784012+thanaism@users.noreply.github.com62784012+thanaism@users.noreply.github.com>
2023-11-25 16:50:05 +0900

    feat: liffの初期化とChakraの導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,41 +1,45 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
 
-import { api } from "~/utils/api";
+import { api } from '~/utils/api';
 
-import "~/styles/globals.css";
-import { useEffect, useState } from "react";
-import { env } from "~/env";
-import type { Liff } from "@line/liff";
+import '~/styles/globals.css';
+import { useEffect, useState } from 'react';
+import { env } from '~/env';
+import { ChakraProvider } from '@chakra-ui/react';
+import type { Liff } from '@line/liff';
 
 const MyApp = ({ Component, pageProps }: AppProps<{ liff: Liff | null; liffError: string | null }>) => {
-  const [liffObject, setLiffObject] = useState<Liff | null>(null);
-  const [liffError, setLiffError] = useState<string | null>(null);
+    const [liffObject, setLiffObject] = useState<Liff | null>(null);
+    const [liffError, setLiffError] = useState<string | null>(null);
 
-  useEffect(() => {
-    // to avoid `window is not defined` error
-    void import("@line/liff")
-      .then((liff) => liff.default)
-      .then((liff) => {
-        console.log("LIFF init...");
-        liff
-          .init({
-            liffId: env.NEXT_PUBLIC_LIFF_ID,
-            withLoginOnExternalBrowser: true,
-          })
-          .then(() => {
-            console.log("LIFF init succeeded.");
-            setLiffObject(liff);
-          })
-          .catch((error: Error) => {
-            console.log("LIFF init failed.");
-            setLiffError(error.toString());
-          });
-      });
-  }, []);
+    useEffect(() => {
+        // to avoid `window is not defined` error
+        void import('@line/liff')
+            .then((liff) => liff.default)
+            .then((liff) => {
+                console.log('LIFF init...');
+                liff.init({
+                    liffId: env.NEXT_PUBLIC_LIFF_ID,
+                    withLoginOnExternalBrowser: true,
+                })
+                    .then(() => {
+                        console.log('LIFF init succeeded.');
+                        setLiffObject(liff);
+                    })
+                    .catch((error: Error) => {
+                        console.log('LIFF init failed.');
+                        setLiffError(error.toString());
+                    });
+            });
+    }, []);
 
-  pageProps.liff = liffObject;
-  pageProps.liffError = liffError;
-  return <Component {...pageProps} />;
+    pageProps.liff = liffObject;
+    pageProps.liffError = liffError;
+    return (
+        <ChakraProvider>
+            <Component {...pageProps} />
+        </ChakraProvider>
+    );
 };
 
 export default api.withTRPC(MyApp);
